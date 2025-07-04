64030494c8d362953a048c6bb576fd5d0e393769
Katanasoldier <akor3618@gmail.com>
2025-07-04 14:56:44 +0200

    Implements timer saving on device storage and adds presets
    Updates main.dart to handle preset initialization, adds shared_preferences as a dependency. Introduces a new presets.json asset containing a Pomodoro preset which is loaded in mian.dart via shared_preferences to not duplicate presets by checking if the application has been already run. Removes positioned from row in top_bar.dart.

--- src/lib/main.dart
+++ src/lib/main.dart
@@ -1,14 +1,33 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+
 import 'package:window_manager/window_manager.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:yourbreak/models/timer_structure.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter/services.dart' show rootBundle;
 
 import 'package:yourbreak/pages/home.dart';
-import 'models/timer_structure.dart';
 
 void main() async {
 
   WidgetsFlutterBinding.ensureInitialized();
+
+
+  await Hive.initFlutter();
+  Hive.registerAdapter(ComplexityAdapter());
+  Hive.registerAdapter(PeriodTypeAdapter());
+  Hive.registerAdapter(TimerPeriodAdapter());
+  Hive.registerAdapter(TimerStructureAdapter());
+
+  await Hive.openBox<TimerStructure>('user_timers');
+  await Hive.openBox<TimerStructure>('preset_timers');
+
+  await checkAndInsertPresets();
+
 
   await windowManager.ensureInitialized();
 
@@ -27,6 +46,7 @@ void main() async {
     await windowManager.focus();
   });
 
+
   runApp(
     ScreenUtilInit(
       designSize: Size(500, 500),
@@ -41,4 +61,41 @@ void main() async {
       ),
     ),
   );
+}
+
+
+Future<void> checkAndInsertPresets() async {
+
+  final prefs = await SharedPreferences.getInstance();
+
+  final bool hasRunBefore = prefs.getBool('hasRunBefore') ?? false;
+  if(!hasRunBefore){
+
+    final presetsBox = Hive.box<TimerStructure>('preset_timers');
+
+    final presetsJson = await rootBundle.loadString('assets/presets.json');
+    final List<dynamic> presetsList = jsonDecode(presetsJson);
+ 
+    for (final preset in presetsList) {
+
+      final pattern = (preset['pattern'] as List<dynamic>).map((item) {
+        return TimerPeriod(
+          periodTime: item['periodTime'] as int,
+          periodType: PeriodType.values.firstWhere((e) => e.name == item['periodType'])
+        );
+      }).toList();
+
+      final structure = TimerStructure(
+        name: preset['name'],
+        complexity: Complexity.values.firstWhere((e) => e.name == preset['complexity']),
+        pattern: pattern,
+      );
+
+      await presetsBox.put(structure.name.toLowerCase(), structure);
+
+    }
+
+    await prefs.setBool('hasRunBefore', true);
+
+  }
 }
