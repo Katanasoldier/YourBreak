559d6a416c00dbfb6a35e4fb31efe5ef0349b8dc
Katanasoldier <akor3618@gmail.com>
2025-08-14 18:39:37 +0200

    Add timer name in TimerRunner page header
    ## Commit Description:
    Replaces predefined text with the actively used timer name inside TimerRunner's page header. Also moves the header up a bit and adds whitespace to organize imports.

--- src/lib/pages/timer_runner.dart
+++ src/lib/pages/timer_runner.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+
 import 'package:yourbreak/constants/color_constants.dart';
 import 'package:yourbreak/constants/font_size_constants.dart';
+
 import 'package:yourbreak/models/timer_structure.dart';
+
 import 'package:yourbreak/templates/base_mixins/page_animation_controller_mixin.dart';
+
 import 'package:yourbreak/templates/base_visuals.dart';
+
 import 'package:yourbreak/templates/buttons/return_button.dart';
 import 'package:yourbreak/templates/timer_runner_components/circular_timer.dart';
 import 'package:yourbreak/templates/timer_runner_components/timer_control_button.dart';
@@ -45,9 +50,9 @@ class TimerRunnerState extends State<TimerRunner> with TickerProviderStateMixin,
                   FittedBox(
                     child: PageHeader(
                       fontSize: FontSizes.pageHeader*0.6,
-                      text: "Pomodoro : Placeholder",
+                      text: widget.timer.name,
                       pageAnimationController: functionalPageAnimationController,
-                      margin: const EdgeInsets.only(top: 37.5, left: 20, right: 20),
+                      margin: const EdgeInsets.only(top: 35, left: 20, right: 20),
                       fontColor: PureColors.white.withValues(alpha: 0.85),
                     ),
                   ),
