8bac029f5a9c64b547167a5b1653eef96f17a7c0
Katanasoldier <akor3618@gmail.com>
2025-08-15 16:37:13 +0200

    Add missing comments for `page_navigation`
    ## Commit Description:
    Adds a brief comment to both functions to describe what they do, how they do it and what arguments they take in to achieve it.

--- src/lib/helper/page_navigation.dart
+++ src/lib/helper/page_navigation.dart
@@ -3,7 +3,11 @@ import 'package:page_transition/page_transition.dart';
 import 'package:yourbreak/constants/animation_constants.dart';
 
 
-
+/// Standardized function to push a new page into view.
+/// Currently the new page slides from the right,
+/// with the ability to drag it away 'iOS' style.
+/// Takes in a required context and newPage, with newPage
+/// being the page that is supposed to be loaded.
 void navigateTo(BuildContext context, Widget newPage) {
   
   Navigator.push(
@@ -24,6 +28,7 @@ void navigateTo(BuildContext context, Widget newPage) {
 
 }
 
+/// Standardized function for popping the current page.
 void popCurrentPage(BuildContext context) {
   
   Navigator.pop(context);
