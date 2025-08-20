dca2c1400075d65a945e38930dc9eab3452f67c1
Katanasoldier <181881376+Katanasoldier@users.noreply.github.com>
2025-08-20 10:21:02 +0200

    Improve comments in misc template files and main
    ## Commit Descriptions
    Improves the rest of the files in templates:
    - `config_page`
    - `page_components`
    - `pop_up`
    - `stat_text`
    and
    - `main`
    Overall improves comments and adds suggestions on how to imrpove the code further, with `TODO` comments.

--- lib/templates/config_page.dart
+++ lib/templates/config_page.dart
@@ -5,8 +5,19 @@ import 'package:yourbreak/constants/animation_constants.dart';
 import 'package:yourbreak/constants/font_size_constants.dart';
 
 
+/// TODO: Rename this more clearly, for example TwoChoiceRedirectionTemplatePage, then remember to update the comments.
+/// 
+/// A template page, designed to be gather more info from the user on where
+/// they want to go. Contains a page header at the top, 2 SquareButtons, each
+/// one redirecting to another page.
+/// At the top a page header, 2 SquareButtons and a return button below them.
+/// Takes in a required:
+/// - header : String, the text displayed at the top
+/// - buttonLeft : Widget Function(AnimationController)
+/// - buttonRight : Widget Function(AnimationController)
 class ConfigPage extends StatefulWidget {
 
+  // TODO: rename this to something better, like headerText, then remember to update the comments.
   final String header;
   final Widget Function(AnimationController) buttonLeft;
   final Widget Function(AnimationController) buttonRight;
