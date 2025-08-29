c03ebf98b31ebb1e99a13018df94ee5ac9b3f8b0
Katanasoldier <181881376+Katanasoldier@users.noreply.github.com>
2025-08-29 11:09:03 +0200

    Add missing `UserStatsStructure` fields and it's `init`'s initialization methods
    ## Commit Description:
    ### `UserStatsStructure`
    Adds missing `lastStreakDate` field in `UserStatsStructure` right under `loginStreak`. Without this, it would't have been possible to track the last time the user logged in, having no real way to actually track the streak.
    Also adds `extendLoginStreak` as a method, that compares `DateTime.now()` against `lastStreakDate`, and if 1 day has passed then increment the streak by 1, but if more than 1 days have passed, then reset the streak to 1.
    Also regenerates it's adapter.
    ### `UserStatsStructureInit`
    Adds a new method called `_putDefaultUser()`, that puts a new entry with a key named `default_user` inside `user_stats` box, with every stat reset and the `lastStreakDate` set to the year 1900 (explain in a second).
    Inside it's`init`, checks the `hasRunBefore` flag, and if it's false then call `_putDefaultUser()`.
    Finally, get the `default_user` from `user_stats` box and call it's `extendLoginStreak()`.
    WHY THE YEAR 1900: `extendLoginStreak` compares the current time with the `lastStreaDate`. If inside `_putDefaultUser` we set it's `lastStreakDate` to `DateTime.now()`, and soon after we call `extendLoginStreak` which also runs `DateTime.now()`, then there won't be a big enough time difference to count the streak.
    Why 1900? Because it's really far back, and since `_putDefaultUser` is ran at the first launch of the app, that means that the user's streak is 1. If `extendLoginStreak` catches that the difference between now and `lastStreakDate` is bigger than 1, then it will reset the streak back to 1. Since 1900 was 125 years ago, it guarantees more than 1 day of difference.

--- lib/models/user_stats_structure/user_stats_structure.dart
+++ lib/models/user_stats_structure/user_stats_structure.dart
@@ -15,23 +15,53 @@ class UserStatsStructure extends HiveObject {
   @HiveField(0)
   double loginStreak;
 
+  /// Holds the DateTime when the streak was last extended.
+  @HiveField(1)
+  DateTime lastStreakDate;
+
   /// How many times in a row the user completed a full work period,
   /// in 1 continuous running timer 
-  @HiveField(1)
+  @HiveField(2)
   double workPeriodStreak;
 
   /// Total time spent in work periods.
   /// This means that if a user runs a timer
   /// on a work period for 5 minutes, this increases
   /// by 5 minutes. ShortBreak (Break) and LongBreak (Rest) don't count.
-  @HiveField(2)
+  @HiveField(3)
   int totalProductiveTime;
 
 
   UserStatsStructure({
     required this.loginStreak,
+    required this.lastStreakDate,
     required this.workPeriodStreak,
     required this.totalProductiveTime
   });
+
+
+  /// Measures how many days have passed since `lastStreakDate`, and
+  /// if more than 1 day passed, reset the streak to 1, because if this ran
+  /// that means the user logged on.
+  /// If only 1 day passed, increment `loginStreak` by 1.
+  /// 
+  /// If 1 or more days have passed, set `lastStreakDate` to `DateTime.now()`.
+  void extendLoginStreak() {
+
+    final int difference = DateTime.now().difference(lastStreakDate).inDays;
+
+    if (difference >= 1) {
+
+      lastStreakDate = DateTime.now();
+      
+      if (difference == 1) {
+        loginStreak++;
+      } else {
+        loginStreak = 1;
+      }
+
+    }
+
+  }
 
 }
